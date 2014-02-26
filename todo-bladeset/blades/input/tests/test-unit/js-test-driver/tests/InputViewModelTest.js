29221687be0d9bf9d34d15ee562e3c052541010f
leggetter <phil@leggetter.co.uk>
2014-02-26 22:21:05 +0000

    input unit test

--- todo-bladeset/blades/input/tests/test-unit/js-test-driver/tests/InputViewModelTest.js
+++ todo-bladeset/blades/input/tests/test-unit/js-test-driver/tests/InputViewModelTest.js
@@ -1,8 +1,8 @@
-var InputViewModelTest = TestCase("InputTest");
+var InputViewModelTest = TestCase( 'InputTest' );
 
 var InputViewModel = require( 'brjstodo/todo/input/InputViewModel' );
 
-InputTest.prototype.testSomething = function() {
+InputViewModelTest.prototype.testSomething = function() {
   var model = new InputViewModel();
-  assertEquals( 'Hello World!', model.message.getValue() );
+  assertEquals( '', model.todoText() );
 };
