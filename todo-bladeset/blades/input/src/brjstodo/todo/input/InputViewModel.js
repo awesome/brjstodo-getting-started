1487c90aa4e6ebbcea216b45173a8ba4888062a0
leggetter <phil@leggetter.co.uk>
2014-02-26 23:47:17 +0000

    trigger event on EventHub when item is entered

--- todo-bladeset/blades/input/src/brjstodo/todo/input/InputViewModel.js
+++ todo-bladeset/blades/input/src/brjstodo/todo/input/InputViewModel.js
@@ -1,17 +1,23 @@
-"use strict";
+'use strict';
 
 var ko = require( 'ko' );
+var ServiceRegistry = require( 'br/ServiceRegistry' );
 
 var ENTER_KEY_CODE = 13;
 
 function InputViewModel() {
-	this.todoText = ko.observable( '' );
+  this.todoText = ko.observable( '' );
+
+  this._eventHub = ServiceRegistry.getService( 'br.event-hub' );
 }
 
-InputViewModel.prototype.keyPressed = function() {
-	if( event.keyCode === ENTER_KEY_CODE ) {
+InputViewModel.prototype.keyPressed = function( item, event ) {
+  if( event.keyCode === ENTER_KEY_CODE ) {
     var todoTextValue = this.todoText();
-    console.log( todoTextValue );
+    var eventData = { title: todoTextValue };
+    this._eventHub.channel( 'todo-list' ).trigger( 'todo-added', eventData );
+
+    this.todoText( '' );
   }
 
   return true;
--- todo-bladeset/blades/input/tests/test-unit/js-test-driver/tests/InputViewModelTest.js
+++ todo-bladeset/blades/input/tests/test-unit/js-test-driver/tests/InputViewModelTest.js
@@ -1,8 +1,53 @@
-var InputViewModelTest = TestCase( 'InputTest' );
+var InputViewModelTest = TestCase( 'InputViewModelTest' );
+
+var ServiceRegistry = require( 'br/ServiceRegistry' );
+
+var fakeEventHub;
+var fakeChannel;
+
+InputViewModelTest.prototype.setUp = function() {
+
+  fakeChannel = {
+    trigger: function( eventName, data ) {
+      // store event name and data
+      this.eventName = eventName;
+      this.data = data;
+    }
+  };
+
+  fakeEventHub = {
+    channel: function( channelName ) {
+      // store the name of the channel
+      this.channelName = channelName;
+      return fakeChannel;
+    }
+  };
+
+  // ensure there isn't already an event-hub registered
+  ServiceRegistry.deregisterService( 'br.event-hub' );
+
+  // Register the fake event hub
+  ServiceRegistry.registerService( 'br.event-hub', fakeEventHub );
+};
 
 var InputViewModel = require( 'brjstodo/todo/input/InputViewModel' );
 
 InputViewModelTest.prototype.testSomething = function() {
   var model = new InputViewModel();
   assertEquals( '', model.todoText() );
+};
+
+InputViewModelTest.prototype.testEnterKeyPressedTriggersEventOnEventHub = function() {
+  // Initialize
+  var testTodoTextValue = 'write some code and test it';
+  var inputViewModel = new InputViewModel();
+  inputViewModel.todoText( testTodoTextValue );
+
+  // Execute test
+  inputViewModel.keyPressed( null, { keyCode: 13 } );
+
+  // Verify
+  assertEquals( 'todo-list', fakeEventHub.channelName );
+  assertEquals( 'todo-added', fakeChannel.eventName );
+  assertEquals( testTodoTextValue, fakeChannel.data.title );
 };
