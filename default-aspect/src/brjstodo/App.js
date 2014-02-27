4b8c67c75061f969206ca8b83280e58b483514c7
leggetter <phil@leggetter.co.uk>
2014-02-27 15:07:28 +0000

    adding view models to aspects

--- default-aspect/src/brjstodo/App.js
+++ default-aspect/src/brjstodo/App.js
@@ -1,7 +1,23 @@
 'use strict';
 
+var InputViewModel = require( 'brjstodo/todo/input/InputViewModel' );
+var ItemsViewModel = require( 'brjstodo/todo/items/ItemsViewModel' );
+
+var KnockoutComponent = require( 'br/knockout/KnockoutComponent' );
+
 var App = function() {
-  // your app code goes here
+debugger;  
+  var inputViewModel = new InputViewModel();
+  var inputComponent =
+    new KnockoutComponent( 'brjstodo.todo.input.view-template', inputViewModel );
+
+  var itemsViewModel = new ItemsViewModel();
+  var itemsComponent =
+    new KnockoutComponent( 'brjstodo.todo.items.view-template', itemsViewModel );
+
+  var todoAppEl = document.getElementById( 'todoapp' );
+  todoAppEl.appendChild( inputComponent.getElement() );
+  todoAppEl.appendChild( itemsComponent.getElement() );    
 };
 
 module.exports = App;
