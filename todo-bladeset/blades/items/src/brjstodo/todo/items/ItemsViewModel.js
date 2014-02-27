2468877fb4d68c5c530595d17ce77347266f5a60
leggetter <phil@leggetter.co.uk>
2014-02-27 01:16:31 +0000

    items blade to use EventHub + tests

--- todo-bladeset/blades/items/src/brjstodo/todo/items/ItemsViewModel.js
+++ todo-bladeset/blades/items/src/brjstodo/todo/items/ItemsViewModel.js
@@ -2,11 +2,22 @@
 
 var ko = require( 'ko' );
 
+var ServiceRegistry = require( 'br/ServiceRegistry' );
+
 function ItemsViewModel() {
   this.todos = ko.observableArray( [
     { title: 'foo' },
     { title: 'bar' }
   ] );
+
+  this._eventHub = ServiceRegistry.getService( 'br.event-hub' );
+
+  // register to recieve events
+  this._eventHub.channel( 'todo-list' ).on( 'todo-added', this._todoAdded, this );
 }
+
+ItemsViewModel.prototype._todoAdded = function( added ) {
+  this.todos.push( added );
+};
 
 module.exports = ItemsViewModel;
--- todo-bladeset/blades/items/tests/test-unit/js-test-driver/tests/ItemsViewModelTest.js
+++ todo-bladeset/blades/items/tests/test-unit/js-test-driver/tests/ItemsViewModelTest.js
@@ -1,8 +1,55 @@
-var ItemsViewModelTest = TestCase( 'ItemsTest' );
+var ItemsViewModelTest = TestCase( 'ItemsViewModelTest' );
+
+var ServiceRegistry = require( 'br/ServiceRegistry' );
+
+var fakeEventHub;
+var fakeChannel;
+
+ItemsViewModelTest.prototype.setUp = function() {
+
+  fakeChannel = {
+    on: function(eventName, callback, context) {
+      // store event name and data
+      this.eventName = eventName;
+      this.callback = callback;
+      this.context = context;
+    }
+  };
+
+  fakeEventHub = {
+    channel: function( channelName ) {
+      // store the name of the channel
+      this.channelName = channelName;
+      return fakeChannel;
+    }
+  };
+
+  // ensure there isn't already an event-hub registered
+  ServiceRegistry.deregisterService( 'br.event-hub' );
+
+  // Register the fake event hub
+  ServiceRegistry.registerService( 'br.event-hub', fakeEventHub );
+};
 
 var ItemsViewModel = require( 'brjstodo/todo/items/ItemsViewModel' );
 
-ItemsViewModelTest.prototype.testSomething = function() {
-  var model = new ItemsViewModel();
-  assertEquals( 'Hello World!', model.message() );
+ItemsViewModelTest.prototype.testitemsBladeListensToItemAddedEvents = function() {
+  var itemsViewModel = new ItemsViewModel();
+
+  assertEquals( fakeEventHub.channelName , 'todo-list' );
+  assertEquals( fakeChannel.eventName , 'todo-added' );
+  assertEquals( fakeChannel.context , itemsViewModel );
+};
+
+ItemsViewModelTest.prototype.testItemsViewModelAddsItemOnTodoAddedEvent = function() {
+  var itemsViewModel = new ItemsViewModel();
+
+  var itemText = 'hello';
+
+  // trigger the callback
+  fakeChannel.callback.call( fakeChannel.context, { title: itemText } );
+
+  // check the item has been added to the end of the list
+  var items = itemsViewModel.todos();
+  assertEquals( itemText, items[ items.length - 1 ].title );
 };
